8a8cd9072af724270125abe51579f4aedbed72d7
Landon Smothers <landonsmothers24@gmail.com>
2021-06-10 12:57:34 -0400

    got music to work

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,13 @@
 // querySelectors
-const musicContainer = document.querySelector('.music-container')
-const playBtn = document.querySelector('#play')
-const prevBtn = document.querySelector('#prev')
-const nextBtn = document.querySelector('#next')
-const audio = document.querySelector('#audio')
-const progress = document.querySelector('.progress')
-const progressContainer = document.querySelector('.progress-container')
-const title  = document.querySelector('#title')
-const cover = document.querySelector('#cover')
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title  = document.querySelector('#title');
+const cover = document.querySelector('#cover');
 
 //song titles
 const songs = ['hey', 'summer', 'ukulele']
@@ -22,7 +22,7 @@ loadSong(songs[songIndex])
 
 function loadSong(song) {
     title.innerText = song
-    audio.src = `/music/music-player_music_/${song}.mp3`
+    audio.src = `/music/music-player_music_${song}.mp3`
     cover.src = `/images/${song}.jpg`
 }
 
@@ -33,6 +33,8 @@ playBtn.querySelector('i.fas').classList.add('fa-pause')
 
 audio.play()
 }
+
+
 function pauseSong() {
     musicContainer.classList.remove('play')
     playBtn.querySelector('i.fas').classList.add('fa-play')
@@ -64,6 +66,20 @@ function nextSong(){
     playSong()
 }
 
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+    const width = this.clientWidth 
+const clickX = e.offsetX
+const duration = audio.duration
+
+audio.currentTime = (clickX / width) * duration
+}
+
 //Event listener
 playBtn.addEventListener('click', () => {
     const isplaying = musicContainer.classList.contains('play')
@@ -77,3 +93,9 @@ playBtn.addEventListener('click', () => {
 // change song events
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', prevSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click',setProgress)
+
+audio.addEventListener('ended', nextSong)
